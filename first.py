8e1155f3bf336a48b7a2dda7feb3a4cb5fc22aad
zitkod <zitkova.dan@gmail.com>
2025-10-02 19:15:12 +0200

    first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé)
+    else:
+        print(f"Číslo {cislo} je liché")
+        
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
