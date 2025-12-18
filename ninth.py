fa08592bbe5c85a3d4c70dd8f28395f4fb314279
zitkod <zitkova.dan@gmail.com>
2025-12-18 11:46:33 +0100

    Add files via upload

---
+++ ninth.py
@@ -0,0 +1,21 @@
+def dec_to_bin(cislo):
+    # funkce prevede cislo na binarni reprezentaci (cislo muze byt str i int!!!)
+    # 7 -> "111"
+    # 5 -> "101"
+    n = int(cislo)
+    if n == 0:
+        return "0"
+    bity = []
+    while n > 0:
+        bity.append(str(n % 2))
+        n //= 2
+    return "".join(reversed(bity))
+
+
+def test_bin_to_dec():
+    assert dec_to_bin("0") == "0"
+    assert dec_to_bin(1) == "1"
+    assert dec_to_bin("100") == "1100100"
+    assert dec_to_bin(101) == "1100101"
+    assert dec_to_bin(127) == "1111111"
+    assert dec_to_bin("128") == "10000000"
